fe33fee213c3744a2a31a27d1303fada06cf2e6e
GarthAnon <114892915+Gnarlsley@users.noreply.github.com>
2024-12-06 12:57:24 -0600

    Update PrintQueue.js

--- PrintQueue.js
+++ PrintQueue.js
@@ -23,31 +23,79 @@ function findMiddle(update){
     return update[midIndex];
 }
 
-function readData(input){
+function performSwap(update, beforeIndex, afterIndex){
+    var temp = update[beforeIndex];
+    update[beforeIndex] = update[afterIndex];
+    update[afterIndex] = temp;
+    return update;
+}
+
+function solvePartTwo(input){
+    var count = 0;
+    var failedUpdates = [];
+    const data = parseData(input);
+    data[1].forEach(update =>{
+        if (!checkCorrectOrder(data[0], update)){
+            //process a check for every rule
+            for (let i = 0; i < data[0].length; i++){
+
+                const [before,after] = data[0][i];
+                //grab the index where the first number appears
+                const beforeIndex = update.indexOf(before);
+                //index for when second number appears
+                const afterIndex = update.indexOf(after);
+
+                //if one or both of the numbers from the current rule are not found in the update, do not provide a false
+                //check that both numbers in the current rule are present in the update and that they violate the rule
+                if (beforeIndex !== -1 && afterIndex !== -1 && beforeIndex > afterIndex){
+                    update = performSwap(update, beforeIndex, afterIndex);
+                    i = 0;
+                }
+            }
+            failedUpdates.push(update);
+        }
+    });
+    failedUpdates.forEach(update =>{
+        if (checkCorrectOrder(data[0], update)){
+            const middle = findMiddle(update);
+            count += middle;
+        }
+        else{
+            console.log("Fail");
+        }
+    });
+    console.log("Answer for part 2: " + count);
+}
+
+function parseData(input){
     //split input rules and page numbers
-    const [rulesStr, updatesStr] = input.split("\r\n\r\n");
+    const [rulesStr, updatesStr] = input.split("\n\n");
     //split the rules by each rule, then each rule by their numbers
     //10|11
     //12|14
     //appears like [[10,11],[12,14]]
-    const rules = rulesStr.split("\r\n").map(rule => rule.split("|").map(Number));
+    const rules = rulesStr.split("\n").map(rule => rule.split("|").map(Number));
     //do the same for the updates
     //10,11,12,14
     //10,12,11
     //[[10,11,12,14],[10,12,11]]
-    const updates = updatesStr.split("\r\n").map(update => update.split(",").map(Number));
-    let sum = 0;
+    const updates = updatesStr.split("\n").map(update => update.split(",").map(Number));
+    return [rules,updates];
+}
 
+function solvePartOne(input){
+    let sum = 0;
+    const data = parseData(input);
     //loop through each update and pass it to the checkOrder function
-    updates.forEach(update =>{
-        if (checkCorrectOrder(rules, update)){
+    data[1].forEach(update =>{
+        if (checkCorrectOrder(data[0], update)){
             //if the update does not violate any rules, find middle number at add to sum
             const middle = findMiddle(update);
             sum += middle;
         }
     });
 
-    return sum;
+    console.log("Answer for part 1: " + sum);
 }
 
 fs.readFile('C:/Users/grran/development/AdventOfCode2024/Day5/puzzle.txt', 'utf8', (err,data)=>{
@@ -55,6 +103,6 @@ fs.readFile('C:/Users/grran/development/AdventOfCode2024/Day5/puzzle.txt', 'utf8
         console.error(err);
         return;
     }
-    var sum = readData(data);
-    console.log(sum);
+    solvePartOne(data);
+    solvePartTwo(data);
 });
